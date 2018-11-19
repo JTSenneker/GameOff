dfbefbf645b5ab462fcf690ae9d9b66e49e1d97e
Unknown <JTSenneker@gmail.com>
2018-11-19 15:48:55 -0500

    Imported projects beginnings

---
+++ GameOff/Assets/Scripts/Card.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CardType
+{
+    NULL,
+    FIRE,
+    ELEC,
+    WATER,
+    WOOD,
+    HEAL
+}
+
+[CreateAssetMenu(fileName = "New Card")]
+public class Card : ScriptableObject {
+
+    public new string name;
+    public string description;
+    public string letterCode;
+    public Sprite image;
+    public int damage;
+    public CardType cardType = CardType.NULL;
+}
