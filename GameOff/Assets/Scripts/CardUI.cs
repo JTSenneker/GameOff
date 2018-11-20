5ccac60fd4a0687b8b814c47824fe610a326194a
Unknown <JTSenneker@gmail.com>
2018-11-19 21:49:01 -0500

    Made a card

---
+++ GameOff/Assets/Scripts/CardUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardUI : MonoBehaviour {
+    public Card card;
+
+    public Text nameText;
+    public Text letterText;
+    public Text descriptionText;
+    public Text damageText;
+    public Image image;
+	// Use this for initialization
+	void Start () {
+
+        nameText.text = card.name;
+        letterText.text = card.letterCode;
+        descriptionText.text = card.description;
+        damageText.text = card.damage.ToString();
+        image.sprite = card.image;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
